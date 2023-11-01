b918894335c4a154a780f46683ccd3a05ac74901
gold <gold@GOLD5809A>
2023-11-01 15:46:03 +0200

    Add project files.

---
+++ wpfData/UserUserControl1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpfData_Step_4.Model;
+using wpfData_Step_4.ViewModel;
+
+namespace wpfData
+{
+    /// <summary>
+    /// Interaction logic for UserUserControl1.xaml
+    /// </summary>
+    public partial class UserUserControl1 : UserControl
+    {
+        public UserUserControl1()
+        {
+            InitializeComponent();
+            UserDB userDB = new UserDB();
+            UserList userList = userDB.SelectAll();
+            usersListView.ItemsSource = userList;
+        }
+    }
+}
---
+++ wpfData/ViewModel/UserDB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using wpfData_Step_4.Model;
+
+namespace wpfData_Step_4.ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User() as BaseEntity;
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = (User)entity;
+            user.Id = (int)reader["PersonID"];
+            user.FirstName = reader["FirstName"].ToString();
+            user.LastName = reader["LastName"].ToString();
+            user.Phone = reader["Phone"].ToString();
+            user.UserName = reader["UserName"].ToString();
+            user.Password = reader["Password"].ToString();
+            user.IsAdmin = bool.Parse(reader["isAdmin"].ToString());
+
+            // השלמת הבאת נתוני עיר לפי קוד העיר
+            int cityID = (int)reader["CityID"];
+            CityDB cityDB = new CityDB();
+            user.City = cityDB.SelectById(cityID);
+
+            return user;
+        }
+
+
+        public UserList SelectAll()
+        {
+            this.command.CommandText = "SELECT * FROM TblUsers";
+            UserList list = new UserList(base.ExecuteCommand());
+            return list;
+        }
+
+        public User SelectById(int id)
+        {
+            command.CommandText = string.Format("SELECT * FROM TblUsers WHERE (ID = {0})", id);
+            UserList list = new UserList(base.ExecuteCommand());
+            if (list.Count == 1)
+                return list[0];
+            return null;
+        }
+
+        public User Login(User user)
+        {
+            command.CommandText = string.Format($"SELECT * FROM TblUsers " 
+                + $"WHERE (UserName = '{user.UserName}') AND ([Password] = '{user.Password}')");
+            UserList list = new UserList(base.ExecuteCommand());
+            if (list.Count == 1)
+                return list[0];
+            return null;
+
+        }
+    }
+}
+
